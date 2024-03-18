1f2d2060890394d86d6077e671bd8b65c3199007
Avinash Ratnam <avinashr@umich.edu>
2024-03-18 13:26:21 -0400

    Restructed components

---
+++ src/app/components/ResearchPostRequestForm/ResearchPostRequestForm.styles.js
@@ -0,0 +1,13 @@
+export const inputClass = "border rounded w-full py-2 px-3 text-black mb-4";
+export const textareaClass = "border rounded w-full py-2 px-3 text-black mb-4";
+export const fileInputClass =
+  "form-input w-full border rounded w-full py-2 px-3 text-black";
+export const dateInputClass =
+  "form-input w-full border rounded w-full py-2 px-3 text-black";
+export const formContainerClass =
+  "flex justify-center items-center min-h-screen";
+export const formSectionClass =
+  "bg-white p-6 rounded shadow-md w-full max-w-md mx-auto mb-4";
+export const buttonClass =
+  "bg-primary hover:bg-primary-dark text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline";
+export const navigationContainerClass = "flex justify-between";

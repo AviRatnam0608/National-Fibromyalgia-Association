ad66c59eae1f3f4df472afcc1c39964422d7ce2b
Avinash Ratnam <avinashr@umich.edu>
2024-04-17 18:56:21 -0400

    Added underline for active tab in navbar

--- src/app/components/NavBar/AdminNavBar.js
+++ src/app/components/NavBar/AdminNavBar.js
@@ -1,22 +1,70 @@
-import { NavBarContainer } from "./NavBar.styles";
+"use client";
 
-function AdminNavBar() {
+import { NavBarContainer } from "./NavBar.styles";
+import { useEffect, useState } from "react";
+
+function ResearcherNavBar() {
+  // State to hold the current path
+  const [currentPath, setCurrentPath] = useState(window.location.pathname);
+
+  // Function to update the current path
+  useEffect(() => {
+    const updatePath = () => {
+      if (window !== undefined) {
+        setCurrentPath(window.location.pathname);
+      }
+    };
+    window.addEventListener("popstate", updatePath);
+    return () => {
+      window.removeEventListener("popstate", updatePath);
+    };
+  }, []);
+
+  // Function to determine if a tab is active
+  const isActive = (path) => {
+    return currentPath === path ? true : false;
+  };
+
   return (
     <nav className={NavBarContainer}>
       <div className="max-w-6xl mx-auto flex justify-between items-center">
         <h1 className="text-xl font-bold">National Fibromyalgia Association</h1>
         <div>
-          <a href="/admin-dashboard" className="px-4 hover:underline">
+          <a
+            href="/login"
+            className={`px-4 hover:underline ${
+              isActive("/login") ? "active: underline" : ""
+            }`}
+          >
+            Login
+          </a>
+          <a
+            href="/admin-dashboard"
+            className={`px-4 hover:underline ${
+              isActive("/admin-dashboard") ? "active: underline" : ""
+            }`}
+          >
             Dashboard
           </a>
-          <a href="/admin-past-studies" className="px-4 hover:underline">
+          <a
+            href="/admin-past-studies"
+            className={`px-4 hover:underline ${
+              isActive("/admin-past-studies") ? "active: underline" : ""
+            }`}
+          >
             Archive
           </a>
-          <a href="/admin-pending" className="px-4 hover:underline">
+          <a
+            href="/admin-pending"
+            className={`px-2 
+            hover:bg-primary 
+            hover:text-white
+            hover:shadow-lg
+            bg-white border border-2 text-primary rounded-lg py-1 ${
+              isActive("/admin-pending") ? "active: underline" : ""
+            }`}
+          >
             Pending Studies
-          </a>
-          <a href="/admin-profiles" className="px-4 hover:underline">
-            Pending Profiles
           </a>
         </div>
       </div>
@@ -24,4 +72,4 @@ function AdminNavBar() {
   );
 }
 
-export default AdminNavBar;
+export default ResearcherNavBar;
--- src/app/components/NavBar/ResearcherNavBar.js
+++ src/app/components/NavBar/ResearcherNavBar.js
@@ -1,21 +1,70 @@
+"use client";
+
 import { NavBarContainer } from "./NavBar.styles";
+import { useEffect, useState } from "react";
 
 function ResearcherNavBar() {
+  // State to hold the current path
+  const [currentPath, setCurrentPath] = useState(window.location.pathname);
+
+  // Function to update the current path
+  useEffect(() => {
+    const updatePath = () => {
+      if (window !== undefined) {
+        setCurrentPath(window.location.pathname);
+      }
+    };
+    window.addEventListener("popstate", updatePath);
+    return () => {
+      window.removeEventListener("popstate", updatePath);
+    };
+  }, []);
+
+  // Function to determine if a tab is active
+  const isActive = (path) => {
+    return currentPath === path ? true : false;
+  };
+
   return (
     <nav className={NavBarContainer}>
       <div className="max-w-6xl mx-auto flex justify-between items-center">
         <h1 className="text-xl font-bold">National Fibromyalgia Association</h1>
         <div>
-          <a href="/login" className="px-4 hover:underline">
+          <a
+            href="/login"
+            className={`px-4 hover:underline ${
+              isActive("/login") ? "active: underline" : ""
+            }`}
+          >
             Login
           </a>
-          <a href="/" className="px-4 hover:underline">
+          <a
+            href="/"
+            className={`px-4 hover:underline ${
+              isActive("/") ? "active: underline" : ""
+            }`}
+          >
             Home
           </a>
-          <a href="/researcher-pending" className="px-4 hover:underline">
+          <a
+            href="/researcher-pending"
+            className={`px-4 hover:underline ${
+              isActive("/researcher-pending") ? "active: underline" : ""
+            }`}
+          >
             Pending
           </a>
-          <a href="/rpr-form" className="px-4 hover:underline">
+          <a
+            href="/rpr-form"
+            className={`px-2 
+              hover:bg-primary 
+              hover:text-white
+              hover:shadow-lg
+              bg-white border border-2 text-primary rounded-lg py-1 ${
+                isActive("/rpr-form") ? "active: underline" : ""
+              }
+              `}
+          >
             New Post
           </a>
         </div>
